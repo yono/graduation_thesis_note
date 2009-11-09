7a310af7e2f333330548f1cdf95f2f8bd83284e8
Yusaku ONO <yono@eva.ie.u-ryukyu.ac.jp>
2009-11-09 20:21:38 +0900

    note/models.py いじって % python manage.py syncdb で Note, MyUser, Group テーブルを作成

--- note/models.py
+++ note/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+## B4, M1, Teacher, etc..
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+class MyUser(User):
+    belong = models.ManyToManyField(Group)
+
+    def __unicode__(self):
+        return self.username
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    locate = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    user = models.ForeignKey(MyUser)
+
+    def __unicode__(self):
+        return self.title
