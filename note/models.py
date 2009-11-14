f9faa200b57987437363ee0938b1e363cf4a8564
Yusaku ONO <yono@eva.ie.u-ryukyu.ac.jp>
2009-11-14 13:09:50 +0900

    Belong の定義変更 トップページにそれっぽい表を作成

--- note/models.py
+++ note/models.py
@@ -11,23 +11,52 @@ class Group(models.Model):
 
 class MyUser(User):
     nick = models.CharField(max_length=100)
-    belong = models.ManyToManyField(Group)
+    belongs = models.ManyToManyField(Group, through='Belong')
     objects = auth_models.UserManager()
 
     def __unicode__(self):
-        return self.username
+        return self.nick
+
+class Belong(models.Model):
+    myuser = models.ForeignKey(MyUser)
+    group = models.ForeignKey(Group)
+    start = models.DateField()
+    end = models.DateField(null=True,blank=True)
+
+    def __unicode__(self):
+        return '%s-%s' % (self.group.name,self.myuser.nick)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+class Comment(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.name
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     locate = models.CharField(max_length=100)
-    date = models.DateTimeField()
+    date = models.DateField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    elapsed_time = models.IntegerField()
     user = models.ForeignKey(MyUser)
+    tag = models.ManyToManyField(Tag)
+    comment = models.ForeignKey(Comment)
 
     def __unicode__(self):
         return self.title
+
 
 from django.contrib import admin
 admin.site.register(MyUser)
 admin.site.register(Note)
 admin.site.register(Group)
+admin.site.register(Belong)
--- note/views.py
+++ note/views.py
@@ -1,13 +1,52 @@
 # Create your views here.
 from django.template import Context, loader
-from graduate.note.models import MyUser,Note
+from graduate.note.models import MyUser,Note, Belong
 from django.http import HttpResponse
 
 def index(request):
     user_list = MyUser.objects.all()
+    years = {}
+    groups = {}
+    for user in user_list:
+        belongs = user.belong_set.all()
+        for belong in belongs:
+            print belong.start.year,belong.group.name
+            years[belong.start.year] = 0
+            groups[belong.group.name] = 0
+    year_list = []
+    for year in years:
+        year_list.append(year)
+    year_list.sort(reverse=True)
+    belong_list = []
+    user_table = []
+    belong_list.insert(0, [''])
+    for group in groups:
+        belong_list.append([group])
+    user_table.append(belong_list)
+    for year in year_list:
+        oneyear_list = Belong.objects.filter(start__year=year)
+        oneyear_users = []
+        for group_list in belong_list:
+            group = group_list[0]
+            oneyear_users.append([])
+            for belong in oneyear_list:
+                if belong.group.name == group:
+                    oneyear_users[-1].append(belong.myuser)
+                elif group == '' and len(oneyear_users[-1]) == 0:
+                    oneyear_users[-1].append(year)
+
+        user_table.append(oneyear_users)
+    
+    for i in user_table:
+        print i
+
+    
     t = loader.get_template('note/index.html')
     c = Context({
         'user_list': user_list,
+        'year_list': year_list,
+        'groups'   : groups,
+        'user_table':user_table,
         })
     return HttpResponse(t.render(c))
 
